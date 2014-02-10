300aba74e0b1555a4dc7dcd9d29703036568a989
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-02-10 15:17:02 +0100

    fix view

--- torino3d/views.py
+++ torino3d/views.py
@@ -1,5 +1,3 @@
-import cgi
-import cgitb; cgitb.enable()  # for troubleshooting
 import json
 import psycopg2
 
@@ -11,22 +9,23 @@ from django.http import HttpResponse
 class Home(View):
     """Render home page"""
 
-    fs = cgi.FieldStorage()
-    if 'bbox_wkt' not in fs:
-        print "Content-type: text/html"
-        print
-        print "<h1>Error</h1>"
-        print "bbox_wkt was not found in URL query string."
-    else:
-        bbox_wkt = fs['bbox_wkt'].value
-
     def get(self, request, *args, **kwargs):
+
+        bbox_wkt = request.GET.get('bbox_wkt')
+
         # Database connection
         conn = psycopg2.connect("host=130.192.92.199 dbname=osm user=osm password=osm")
+        
         # Database cursor
         cur = conn.cursor()
         # Query
-        cur.execute('SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' As type, array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, ST_AsGeoJSON(ST_Transform(lg.the_geom,4326))::json As geometry, row_to_json((SELECT l FROM (SELECT height) As l)) As properties FROM torino_building As lg WHERE ST_Within(lg.the_geom, ST_Transform(ST_GeomFromText(%s,4326),27700)) LIMIT 100000) As f )  As fc;' % bbox_wkt)
+        cur.execute('SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' As type, \
+         array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, \
+          ST_AsGeoJSON(ST_Transform(lg.the_geom,4326))::json As geometry, \
+          row_to_json((SELECT l FROM (SELECT height) As l)) As properties FROM \
+           torino_building As lg WHERE ST_Within(lg.the_geom, \
+            ST_Transform(ST_GeomFromText(%s,4326),27700)) LIMIT 100000) As f )  \
+            As fc;' % bbox_wkt)
         array = cur.fetchall()
 
         # Return data as GeoJSON
